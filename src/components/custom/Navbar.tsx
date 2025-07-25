4b5f31f244a9b7b02fa6571892bc525d64e8d5f3
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-07-25 11:52:53 +0530

    Working On UI

---
+++ src/components/custom/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full h-14 px-4 border-b shadow-sm bg-background flex items-center">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center gap-x-4">
+          <Link href={"/"}>
+            <h1 className="font-semibold text-xl">Todo Application</h1>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
