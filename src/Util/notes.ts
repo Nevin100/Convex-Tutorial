2f3b4a8a0447162bd0093fef71f1c8aefdeeff22
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-07-25 12:09:02 +0530

    Setting Convex Successfully

---
+++ src/Util/notes.ts
@@ -0,0 +1,14 @@
+// It consists my notes and important points for future reference
+
+// Installation and Setup :
+// 1.) Convex Installation :  npm install convex
+// 2.) Convex Dev Env Setup : npx convex dev --> Select the required options in your bash/terminal
+
+// Now Functions :
+// -> A new folder 'convex' is created where you will write the controller functions for your APIs
+
+//Terminologies :
+// 1.) Query : are the bread and butter of your backend api. they fetch data from the database, check authentication or perform bussiness logic and return back to client application.
+// 2.) Mutations : it inserts , updates and deletes data from the database.
+// 3.) Actions : It basically calls the third party services like Razorpay or stripe.
+// 4.) HTTP Actions : allows to build an HTTP API right in convex usually preferred while setting some web hooks.
